e0fdea28c7814a5fa3aaed7444cee979e9c7d200
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-04-19 13:47:54 +0300

    Lab problem 3 added

---
+++ 01_Basic_Syntax_Lab/03_excellent_grade.js
@@ -0,0 +1,10 @@
+function is_excellent_grade(grade) {
+    if (grade >= 5.50) {
+        console.log('Excellent');
+    }else{
+        console.log('Not excellent')
+    }
+}
+
+is_excellent_grade(6)
+is_excellent_grade(4.5)
