c42dcf8b3194b0d0a18e2eb0ef512ebaf32ba8f2
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-04-19 13:31:14 +0300

    Lab problem 2 added

---
+++ 01_Basic_Syntax_Lab/02_student_information.js
@@ -0,0 +1,7 @@
+function student_info(name, age, av_grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${av_grade.toFixed(2)}`);
+}
+
+student_info('John', 15, 5.54678)
+student_info('Steve', 16, 2.1426)
+student_info('Marry', 12, 6.00)
