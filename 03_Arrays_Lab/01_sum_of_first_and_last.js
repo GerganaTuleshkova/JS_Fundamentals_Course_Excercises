ef65ef1b272f69f5cbd0102cde5b3a7b3412f19f
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-04 20:19:42 +0300

    Problem 1 added

---
+++ 03_Arrays_Lab/01_sum_of_first_and_last.js
@@ -0,0 +1,10 @@
+function sumFirstLast(numbers){
+    
+    let sum = numbers[0] + numbers[numbers.length-1];
+    console.log(sum)
+
+}
+
+sumFirstLast([20, 30, 40])
+sumFirstLast([10, 17, 22, 33])
+sumFirstLast([11, 58, 69])
