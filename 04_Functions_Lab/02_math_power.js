ddd9eeaadba7b6b5cb1070506c896285201aba96
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-06 17:33:51 +0300

    Problem added

---
+++ 04_Functions_Lab/02_math_power.js
@@ -0,0 +1,19 @@
+// function mathPower(number, power) {
+//     let result = 1;
+    
+//     for (i = 0; i < power; i++) {
+//         result *= number;
+//     }
+//     console.log(result)
+// }
+
+function mathPower(number, power) {
+        
+        console.log(number ** power)
+    }
+
+mathPower(2, 8);
+mathPower(3, 4);
+mathPower(5, 0);
+
+
