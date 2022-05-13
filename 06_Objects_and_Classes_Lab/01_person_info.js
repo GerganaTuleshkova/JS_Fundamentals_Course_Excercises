6c3f0f1a6b8946f299766a9d36004d3ce06ad1e6
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-13 16:30:28 +0300

    Problem added

---
+++ 06_Objects_and_Classes_Lab/01_person_info.js
@@ -0,0 +1,12 @@
+function personInfo(firstName, lastName, age) {
+    let personDetails = {
+        firstName: firstName, 
+        lastName: lastName, 
+        age: age};
+    return personDetails;
+}
+
+console.log(personInfo("Peter", 
+"Pan",
+"20"
+))
