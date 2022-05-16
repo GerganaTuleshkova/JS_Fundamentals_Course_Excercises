9418d2298d0a34dbdd93ee4e6be1fc17ff2be633
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-16 15:28:00 +0300

    Problem added

--- 06_Objects_and_Classes_Lab/05_cats.js
+++ 06_Objects_and_Classes_Lab/05_cats.js
@@ -1,20 +1,18 @@
-class Cat {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    meow() {
-        console.log(`${this.name}, age ${this.age} says Meow`);
-    }
-}
-
 function cats(input) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+    
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
     for (let element of input) {
-        let name, age = element.split(' ');
-        // let details = element.split(' ');
-        // let name = details[0];
-        // let age = details[1];
+        let details = element.split(' ');
+        let name = details[0];
+        let age = details[1];
 
         let cat = new Cat(name, age);
         cat.meow()
---
+++ 07_Associative_Arrays_Lab/01_phone_book.js
@@ -0,0 +1,18 @@
+function phoneBook(detailsAsList) {
+    let myPhoneBook = {
+    }
+    for (let element of detailsAsList) {
+        let [name, number] = element.split(' ');
+        myPhoneBook[name] = number
+    }
+
+    for (person in myPhoneBook) {
+        console.log(`${person} - > ${myPhoneBook[person]}`)
+    }
+}
+
+phoneBook(['Tim 0834212554',
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344']
+)
